a43d0131cbfd777c923dcc590032dabb46bb6d65
Arseni Skobelev <arseni.skobelev@ikomm.no>
2023-07-10 15:54:23 +0200

    Added session creation (auth routes..)
    added node_modules to .gitignore. big smol mistake.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,4 @@
 ﻿using cliph.Models.Http;
-using cliph.Models.Requests;
-using cliph.Models.Responses;
 using cliph.Models.User;
 using cliph.Services.UserService;
 using Microsoft.AspNetCore.Authorization;
@@ -17,32 +15,6 @@ public class UserController : ControllerBase
     public UserController(IUserService userService)
     {
         _userService = userService;
-    }
-    
-    [HttpPost]
-    public async Task<JsonResult> CreateUser([FromBody] UserRequest newUserData)
-    {
-        try
-        {
-            if (Request.Headers.TryGetValue("x-cliph-key", out var apiKeyHeaderValue))
-            {
-                if (string.IsNullOrWhiteSpace(apiKeyHeaderValue))
-                    return new JsonResult(new Response(false, "Unable to verify administrator authentication token"));
-
-                var managedUserResult = await _userService.CreateUser(newUserData, apiKeyHeaderValue!);
-
-                return new JsonResult(new Response<UserResponse>(true, managedUserResult, "Created a new user successfully"));
-            }
-
-            var adminUserResult = await _userService.CreateUser(newUserData);
-        
-            return new JsonResult(new Response<UserResponse>(true, adminUserResult, "Created a new user successfully"));
-        }
-        catch (Exception e)
-        {
-            Response.StatusCode = 500;
-            return new JsonResult(new Response(false, e.Message));
-        }
     }
     
     [Authorize]
