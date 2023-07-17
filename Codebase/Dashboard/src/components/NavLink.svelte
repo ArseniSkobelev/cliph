48ab0b1b853a24d9d840a1db14aca3b78f471422
unknown <arseni.skobelev0@gmail.com>
2023-07-17 19:12:17 +0200

    Started rewriting the dashboard after a simple redesign

---
+++ Codebase/Dashboard/src/components/NavLink.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let text: string = "Navigation link";
+    export let href: string = "/";
+    export let active: boolean = false;
+</script>
+
+<a {href} class="flex flex-row gap-x-4 items-center">
+    <slot/>
+    <span class='{active ? "font-bold text-brand-500" : "text-dark-gray font-regular"}'>{text}</span>
+</a>
--- dashboard/src/components/TopBar.svelte
+++
@@ -1,3 +0,0 @@
-<div class="w-full bg-theme-primary-accent-700 z-0 text-theme-light-text p-4">
-    <h1>I am the topbar</h1>
-</div>
